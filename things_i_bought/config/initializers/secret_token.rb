97462ba55e3fbcdb65ee85aad636344df2b08638
lanpm <lanvnjp@gmail.com>
2011-12-17 08:32:12 +0700

    second commit

---
+++ things_i_bought/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = '7bab82812de286b3b608d2adf2a2bf111b13e6c1f4bbd777f9d294f34a9383b7858175e96a59d1b4e5a44e243d8ed511657af75036864d5089fce1e015e42f2c'
